e823bfa74cedfa81970f1b49843c5736812c22d6
Adam Bohannon <abo46n2@gmail.com>
2017-10-30 15:16:59 -0700

    Yet another approach

--- script.js
+++ script.js
@@ -11,48 +11,8 @@ const app = (body) => {
     return numbers.push(d[1])
   })
   const maxValue = d3.max(numbers)
-  console.log(maxValue)
   const minDate = new Date(data[0][0])
   const maxDate = new Date(data[data.length - 1][0])
-
-  const dimensions = {
-    width: 600,
-    height: 400,
-    margin: 40,
-    innerWidth: 520,
-    innerHeight: 320,
-    barMargin: 5
-  }
-
-  const xScale = d3.scaleLinear()
-    .domain([0, data.length])
-    .range([0, dimensions.innerWidth])
-
-  const yScale = d3.scaleLinear()
-    .domain([0, maxValue])
-    .range([0, dimensions.innerHeight])
-
-  const yAxisScale = d3.scaleLinear()
-    .domain([maxValue, 0])
-    .range([0, dimensions.innerHeight])
-
-  const chart = d3.select('.chart')
-    .append('svg')
-    .attrs({
-      width: dimensions.width,
-      height: dimensions.height
-    })
-
-  const yAxis = d3.axisLeft(yAxisScale)
-    .tickSizeInner(-dimensions.innerWidth)
-    .tickPadding(5)
-
-  chart.append('g')
-    .attrs({
-      class: 'axis axis-y',
-      transform: 'translate(' + dimensions.margin + ', ' + dimensions.margin + ')'
-    })
-    .call(yAxis)
 
   d3.select('.title')
     .append('h2')
@@ -61,8 +21,54 @@ const app = (body) => {
   d3.select('.description')
     .append('p')
     .text(description)
+
+  const svg = d3.select('svg')
+  const margin = {top: 20, right: 20, bottom: 30, left: 40}
+  const width = +svg.attr('width') - margin.left - margin.right
+  const height = +svg.attr('height') - margin.top - margin.bottom
+
+  const x = d3.scaleBand().rangeRound([0, width]).padding(0.3)
+  const y = d3.scaleLinear().rangeRound([height, 0])
+
+  const g = svg.append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+  x.domain(data.map((d) => {
+    return d[0]
+  }))
+
+  y.domain([0, d3.max(data, (d) => {
+    return d[1]
+  })])
+
+  console.log(minDate, maxDate)
+
+  g.append('g')
+    .attr('class', 'axis axis--x')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(d3.axisBottom(x).ticks(d3.timeYears(5)))
+
+  g.append('g')
+    .attr('class', 'axis axis--y')
+    .call(d3.axisLeft(y).ticks(10))
+    .append('text')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', 6)
+    .attr('dy', '0.71em')
+    .attr('text-anchor', 'end')
+    .text('Frequency')
+
+  g.selectAll('.bar')
+    .data(data)
+    .enter().append('rect')
+    .attr('class', 'bar')
+    .attr('x', (d) => x(d[0]))
+    .attr('y', (d) => y(d[1]))
+    .attr('width', x.bandwidth())
+    .attr('height', (d) => height - y(d[1]))
 }
 
+// Fetch the data
 fetch(dataUrl)
   .then((response) => {
     if (response.status !== 200) {
